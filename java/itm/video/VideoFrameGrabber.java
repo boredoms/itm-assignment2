a2e17919f2c52cfe9d0a43ea53d14e39798c8e8e
miniplane <jasmin.min.wagner@gmail.com>
2017-05-26 14:38:35 +0200

    Video Frame Grabber

--- java/itm/video/VideoFrameGrabber.java
+++ java/itm/video/VideoFrameGrabber.java
@@ -9,6 +9,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import com.xuggle.mediatool.IMediaReader;
+import com.xuggle.mediatool.ToolFactory;
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
+
+import com.xuggle.ferry.JNILibraryLoader;
+import com.xuggle.xuggler.Global;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IPacket;
+import com.xuggle.xuggler.IPixelFormat;
+import com.xuggle.xuggler.IPixelFormat.Type;
+import com.xuggle.xuggler.IStream;
+import com.xuggle.xuggler.IStreamCoder;
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IVideoPicture;
+import com.xuggle.xuggler.IVideoResampler;
+import com.xuggle.xuggler.Utils;
+
+
 /**
  * 
  * This class creates JPEG thumbnails from from video frames grabbed from the
@@ -98,6 +120,81 @@ public class VideoFrameGrabber {
 		// ***************************************************************
 		// Fill in your code here!
 		// ***************************************************************
+
+		// that helped me: https://github.com/artclarke/xuggle-xuggler/blob/master/src/com/xuggle/xuggler/demos/DecodeAndCaptureFrames.java
+		try {
+			
+			IContainer container = IContainer.make();
+
+			int containerExists = container.open(input.getAbsolutePath(), IContainer.Type.READ, null);
+
+			if (containerExists < 0)
+				throw new RuntimeException("Failed to open media file");
+			
+			int i = 0;
+			IStreamCoder streamCoder = null;
+
+			int streamnum = container.getNumStreams();
+			
+			for (; i < streamnum; i++) {
+				streamCoder = container.getStream(i).getStreamCoder();
+
+				if (streamCoder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO)
+					break;
+			}
+			
+			streamCoder.open();
+			boolean resample = false;
+			IVideoResampler videoResampler = null;
+			
+			if (streamCoder.getPixelType() != IPixelFormat.Type.BGR24) {
+				resample = true;
+				videoResampler = IVideoResampler.make(streamCoder.getWidth(),
+						streamCoder.getHeight(), IPixelFormat.Type.BGR24,
+						streamCoder.getWidth(), streamCoder.getHeight(),
+						streamCoder.getPixelType());
+			}
+
+			IPacket packet = IPacket.make();
+			ArrayList<IVideoPicture> savedFrames = new ArrayList<IVideoPicture>();
+			
+
+			while (container.readNextPacket(packet) >= 0) {
+
+				if (packet.getStreamIndex() != i)
+					continue;
+
+				IVideoPicture pic = IVideoPicture.make(streamCoder.getPixelType(),
+						streamCoder.getWidth(), streamCoder.getHeight());
+
+				int offset = 0;
+
+				while (offset < packet.getSize()) {
+
+					offset += streamCoder.decodeVideo(pic, packet, offset);
+
+					if (!pic.isComplete())
+						continue;
+					
+					if (resample) {
+							IVideoPicture pic2 = IVideoPicture.make(
+									IPixelFormat.Type.BGR24, streamCoder.getWidth(),
+									streamCoder.getHeight());
+							videoResampler.resample(pic2, pic);
+							savedFrames.add(pic2);
+					} else {
+						savedFrames.add(pic);
+					}		
+
+				}
+			}
+
+			@SuppressWarnings("deprecation")
+			BufferedImage img = com.xuggle.xuggler.Utils.videoPictureToImage(savedFrames.get(savedFrames.size()/2)); // get Frame in the middle of savedFrames
+			ImageIO.write(img, "JPEG", outputFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		return outputFile;
 
